da803b8402ebcd9bfa1e040e5973607a5c590f16
Shubhi-Sri <shubhisrivastava514@gmail.com>
2025-09-17 14:43:55 +0000

    new changes made

--- variables.sh
+++ variables.sh
@@ -3,14 +3,12 @@
 #
 # << multiline comment
 << comment
-
 anything wwritten here will not execute 
 comment
-
 name="Shubhi"
 name2="Srivastava"
 echo "Name is $name" "$name2"
-
 echo"enter you partner name :) "
 read partner
 echo " hey Shubhi, You love $partner, May $name and $partner live together"
+echo"Lets add to github:"
