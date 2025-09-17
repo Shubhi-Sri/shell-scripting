04b148f17b621718b87c435e4145daf88d003d01
Shubhi-Sri <shubhisrivastava514@gmail.com>
2025-09-17 15:23:53 +0000

    space added

--- variables.sh
+++ variables.sh
@@ -2,14 +2,12 @@
 # lets learn variables
 #
 # << multiline comment
-<< comment
-anything wwritten here will not execute 
-comment
-echo"hello"
+
+echo "hello"
 name="Shubhi"
 name2="Srivastava"
 echo "Name is $name" "$name2"
-echo"enter you partner name :) "
+echo "enter you partner name :) "
 read partner
 echo " hey Shubhi, You love $partner, May $name and $partner live together"
-echo"Lets add to github:"
+echo "Lets add to github:"
