c5ab7f97cbe1f7e0d2a50c9781bf5a7fc19fb14c
Shubhi-Sri <shubhisrivastava514@gmail.com>
2025-09-17 13:46:52 +0000

    Add shell script

---
+++ variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# lets learn variables
+#
+# << multiline comment
+<< comment
+
+anything wwritten here will not execute 
+comment
+
+name="Shubhi"
+name2="Srivastava"
+echo "Name is $name" "$name2"
+
+echo"enter you partner name :) "
+read partner
+echo " hey Shubhi, You love $partner, May $name and $partner live together"
